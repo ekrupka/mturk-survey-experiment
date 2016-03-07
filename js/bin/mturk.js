67655aa1da91cc41fe8c2b87b00cfc00911c6739
jczetta <jczetta@gmail.com>
2016-03-07 11:29:59 -0500

    Edit bonus task logic flow and text so it is correct.

--- js/bin/mturk.js
+++ js/bin/mturk.js
@@ -790,12 +790,10 @@ Handlebars.registerHelper('randomInput', function(context, options) {
 		if ( yourTokenVal === 10 ) {
 			out += makeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' to ';
 		} else if ( yourTokenVal === 0 ) {
-			out += makeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' to ';
+			out += takeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' from ';
 		} else {
 			if ( count >= 5 ) {
-
-					out += makeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' to ';
-
+				out += makeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' to ';
 			} else {
 				out += takeTrans + spanUL + curToken + spanEnd + ' <span class="update-token">tokens' + spanEnd + spanEnd + ' from ';
 			}
