a27a03a8c2211c4ac6409577d300889efa4de1b4
U-NINI-CHIMERA\Daphne <daphnec@umich.edu>
2016-06-17 22:47:50 -0400

    Fixed URLs to images of primes.

--- js/bin/mturk.js
+++ js/bin/mturk.js
@@ -314,7 +314,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: '/images/male1.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male1.png',
 					alt: 'male photo',
 					id: 'male1',
 					label: 'I think this person is more attractive',
@@ -322,7 +322,7 @@ var contextPol = {
 					value: '0'
 				},
 				{
-					src: '/images/male2.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male2.png',
 					alt: 'male Photo',
 					id: 'male2',
 					label: 'I think this person is more attractive',
@@ -334,7 +334,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: '/images/male3.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male3.png',
 					alt: 'male photo',
 					id: 'male3',
 					label: 'I think this person is more attractive',
@@ -342,7 +342,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: '/images/male4.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male4.png',
 					alt: 'Male Photo',
 					id: 'male4',
 					label: 'I think this person is more attractive',
@@ -354,7 +354,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: '/images/female1.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/female1.png',
 					alt: 'female photo',
 					id: 'female1',
 					label: 'I think this person is more attractive',
@@ -362,7 +362,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: '/images/female2.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/female2.png',
 					alt: 'female Photo',
 					id: 'female2',
 					label: 'I think this person is more attractive',
@@ -374,7 +374,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: '/images/female3.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/female3.png',
 					alt: 'female photo',
 					id: 'female3',
 					label: 'I think this person is more attractive',
@@ -382,7 +382,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: '/images/female4.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/female4.png',
 					alt: 'female Photo',
 					id: 'female4',
 					label: 'I think this person is more attractive',
@@ -394,7 +394,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: '/images/male5.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male5.png',
 					alt: 'male photo',
 					id: 'male5',
 					label: 'I think this person is more attractive',
@@ -402,7 +402,7 @@ var contextPol = {
 					value: '0'
 				},
 				{
-					src: '/images/male6.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/male6.png',
 					alt: 'male Photo',
 					id: 'male6',
 					label: 'I think this person is more attractive',
@@ -433,7 +433,7 @@ var contextVotingLine = {
 		{
 			images: [
 				{
-					src: '/images/line1.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/line1.png',
 					alt: 'Voting line photo',
 					id: 'voting-line-1',
 					label: 'I think this line is longest',
@@ -441,7 +441,7 @@ var contextVotingLine = {
 					value: 0
 				},
 				{
-					src: '/images/line2.png',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/line2.png',
 					alt: 'Voting Line Photo',
 					id: 'voting-line-2',
 					label: 'I think this line is longest',
@@ -470,25 +470,25 @@ var contextStates = {
 
 	states: [
 		{
-			src: '/images/NorthCarolina.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/NorthCarolina.png',
 			alt: 'North Carolina photo',
 			name: 'north-carolina',
 			state: 'North Carolina'
 		},
 		{
-			src: '/images/Georgia.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/Georgia.png',
 			alt: 'Georgia photo',
 			name: 'georgia',
 			state: 'Georgia'
 		},
 		{
-			src: '/images/Florida.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/Florida.png',
 			alt: 'Florida photo',
 			name: 'florida',
 			state: 'Florida'
 		},
 		{
-			src: '/images/Ohio.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/base_norms_unconditional/images/Ohio.png',
 			alt: 'Ohio photo',
 			name: 'ohio',
 			state: 'Ohio'
