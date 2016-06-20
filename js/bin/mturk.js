1d7c06d6fadf1cdd6ce5634f24a8b79049b11535
U-NINI-CHIMERA\Daphne <daphnec@umich.edu>
2016-06-20 11:43:02 -0400

    Changing URLs to appropriate address.

--- js/bin/mturk.js
+++ js/bin/mturk.js
@@ -311,7 +311,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/BillClinton.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/BillClinton.jpg',
 					alt: 'Bill Clinton photo',
 					id: 'bill-clinton',
 					label: 'Bill Clinton',
@@ -319,7 +319,7 @@ var contextPol = {
 					value: '0'
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/RonaldReagan.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/RonaldReagan.jpg',
 					alt: 'Ronald Reagan Photo',
 					id: 'ronald-reagan',
 					label: 'Ronald Reagan',
@@ -331,7 +331,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/MitchMcConnell.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/MitchMcConnell.jpg',
 					alt: 'Mitch McConnell photo',
 					id: 'mitch-mcconnell',
 					label: 'Mitch McConnell',
@@ -339,7 +339,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/HarryReid.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/HarryReid.jpg',
 					alt: 'Harry Reid Photo',
 					id: 'harry-reid',
 					label: 'Harry Reid',
@@ -351,7 +351,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/JanBrewer.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/JanBrewer.jpg',
 					alt: 'Jan Brewer photo',
 					id: 'jan-brewer',
 					label: 'Jan Brewer',
@@ -359,7 +359,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/KathleenSebelius.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/KathleenSebelius.jpg',
 					alt: 'Kathleen Sebelius Photo',
 					id: 'kathleen-sebelius',
 					label: 'Kathleen Sebelius',
@@ -371,7 +371,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/MicheleBachmann.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/MicheleBachmann.jpg',
 					alt: 'Michelle Bachmann photo',
 					id: 'michelle-bachmann',
 					label: 'Michelle Bachmann',
@@ -379,7 +379,7 @@ var contextPol = {
 					value: '1'
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/NancyPelosi.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/NancyPelosi.jpg',
 					alt: 'Nancy Pelosi  Photo',
 					id: 'nancy-pelosi',
 					label: ' Nancy Pelosi',
@@ -391,7 +391,7 @@ var contextPol = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/JoeBiden.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/JoeBiden.jpg',
 					alt: 'Joe Biden photo',
 					id: 'joe-biden',
 					label: 'Joe Biden',
@@ -399,7 +399,7 @@ var contextPol = {
 					value: '0'
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/DickCheney.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/DickCheney.jpg',
 					alt: 'Dick Cheney Photo',
 					id: 'Dick-Cheney',
 					label: 'Dick Cheney',
@@ -430,7 +430,7 @@ var contextVotingLine = {
 		{
 			images: [
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/VotingLine1.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/VotingLine1.jpg',
 					alt: 'Voting line photo',
 					id: 'voting-line-1',
 					label: 'I think this voting line is longest',
@@ -438,7 +438,7 @@ var contextVotingLine = {
 					value: 0
 				},
 				{
-					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/VotingLine2.jpg',
+					src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/VotingLine2.jpg',
 					alt: 'Voting Line Photo',
 					id: 'voting-line-2',
 					label: 'I think this voting line is longest',
@@ -467,25 +467,25 @@ var contextStates = {
 
 	states: [
 		{
-			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/NorthCarolina.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/NorthCarolina.png',
 			alt: 'North Carolina photo',
 			name: 'north-carolina',
 			state: 'North Carolina'
 		},
 		{
-			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/Georgia.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/Georgia.png',
 			alt: 'Georgia photo',
 			name: 'georgia',
 			state: 'Georgia'
 		},
 		{
-			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/Florida.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/Florida.png',
 			alt: 'Florida photo',
 			name: 'florida',
 			state: 'Florida'
 		},
 		{
-			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/images/Ohio.png',
+			src: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/images/Ohio.png',
 			alt: 'Ohio photo',
 			name: 'ohio',
 			state: 'Ohio'
@@ -853,31 +853,31 @@ $(function() {
 		// templates urls for requesting template files and names to store templates
 		this.templateNames = [
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/intro.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/intro.html',
 				dataName: 'intro'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/question.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/question.html',
 				dataName: 'question'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/two-question.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/two-question.html',
 				dataName: 'twoPic'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/one-pic.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/one-pic.html',
 				dataName: 'onePic'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/one-pic-radio-opts.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/one-pic-radio-opts.html',
 				dataName: 'onePicInput'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/tokens.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/tokens.html',
 				dataName: 'tokenBase'
 			},
 			{
-				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_random_matched/templates/survey.html',
+				url: 'https://rawgit.com/ekrupka/mturk-survey-experiment/treat_norms_framed_random/templates/survey.html',
 				dataName: 'survey'
 			}
 		];
